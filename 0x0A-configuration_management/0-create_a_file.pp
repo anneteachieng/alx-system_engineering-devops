0062c1666ef4e3c6e86d259a433f0f30f0eb1b3e
anneteachieng <achiengomondi02@gmail.com>
2024-04-21 23:16:24 -0700

    puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+#!/usr/bin/pup
+# creates a file
+
+file { '/tmp/school':
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
